ab5671b4829884a1a0df2645f55d3b4bfc255e9c
liugegewu <1090230878@qq.com>
2022-06-17 08:52:32 +0800

    初始化提交。

---
+++ pages/index/index.js
@@ -0,0 +1,72 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    userAddress:{n:'我的',w:'你的'},
+    showAddress:''
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  //获取地址
+  getUserAddress(){
+    wx.getLocation({
+      type:'wgs84',
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          userAddress:{n:res.latitude,w:res.longitude},
+          showAddress:res.latitude+'N，'+res.longitude+'W'
+        })
+      }
+    })
+  },
+
+  //用户页面跳转
+  returnUserPage:()=>{
+    wx.navigateTo({
+      url:'/pages/userPage/userPage'
+    })
+  }
+
+})
