6e41f90dda1f14ba1269a1963dc05d412577d5b7
‘liugegewu’ <1090230878@qq.com>
2022-06-21 15:24:06 +0800

    主页显示优化。

--- page/pages/index/index.js
+++ page/pages/index/index.js
@@ -4,12 +4,11 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    canIUseOpenData: false, // 如需尝试获取用户信息可改为false
     showAddress:'',
     isShowImg:false,
     time1:null
@@ -24,6 +23,7 @@ Page({
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true,
+        // canIUseOpenData:false,
         isShowImg:true
       })
     }
@@ -33,14 +33,26 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-  
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true,
+        // canIUseOpenData:false,
+        isShowImg:true
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true,
+        // canIUseOpenData:false,
+        isShowImg:true
+      })
+    }
   },
 
   getUserProfile(e) {
@@ -48,20 +60,11 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
       }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   },
 
